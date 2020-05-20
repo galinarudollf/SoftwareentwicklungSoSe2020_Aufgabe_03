1638e1eb383310c1eb1ff7f149866e79768d0e39
galinarudollf <49473764+galinarudollf@users.noreply.github.com>
2020-05-20 09:36:35 +0200

    nur die Funktion Flash fertig

---
+++ Aufgabe_01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Aufgabe_01
+{
+    class Program
+    {
+        private static readonly int langePause = 1000;
+        private static readonly int kurzePause = 300;
+        private static readonly int punkt = 100;
+        private static readonly int strich = 300;
+
+        static void Flash(int ms)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.Clear();
+            System.Threading.Thread.Sleep(ms);
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+        }
+        static void Main(string[] args)
+        {
+            String umzuwandeln = "Zeichenkette";
+            Console.WriteLine(umzuwandeln);
+            foreach (char zeichen in umzuwandeln)
+            {
+            }
+        }
+    }
+}
